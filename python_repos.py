5cea347a075006b1e9bee5c5352aac5307869a03
caizimu <155803924+jackyzimu@users.noreply.github.com>
2025-05-25 12:49:09 +0800

    Add files via upload

--- python_repos.py
+++ python_repos.py
@@ -16,6 +16,16 @@ print(f"已返回存储库: {len(repo_dicts)}")
 
 # 研究第一个仓库。
 repo_dict = repo_dicts[0]
-print(f"\nKeys: {len(repo_dict)}")
-for key in sorted(repo_dict.keys()):
-	print(key)
+
+print("\n关于第一个存储库的选定信息：")
+print(f"名称：{repo_dict['name']}")
+print(f"所有者: {repo_dict['owner']['login']}")
+print(f"评级: {repo_dict['stargazers_count']}")
+print(f"储存库: {repo_dict['html_url']}")
+print(f"创建时间: {repo_dict['created_at']}")
+print(f"最后一次更新的时间： {repo_dict['updated_at']}")
+print(f"描述: {repo_dict['description']}")
+
+#print(f"\nKeys: {len(repo_dict)}")
+#for key in sorted(repo_dict.keys()):
+#	print(key)
